02641cb7c1bff5b35eda4cde99aa2a7e75b49008
cakeinpanic <cakeinpanic@gmail.com>
2026-01-12 14:06:12 +0200

    build: provide base property to iframe build as well

--- vite.config.iframe.js
+++ vite.config.iframe.js
@@ -1,27 +1,33 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig({
-  plugins: [svgr(), react()],
-  server: {
-    port: 3008,
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'iframe.html',
+export default defineConfig(async ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const base = env['VITE_BASE_URL'];
+
+  return {
+    base,
+    plugins: [svgr(), react()],
+    server: {
+      port: 3008,
     },
-  },
-  resolve: {
-    alias: {
-      assets: path.resolve(__dirname, './assets'),
-      '@': path.resolve(__dirname, './src'),
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: 'iframe.html',
+      },
     },
-  },
-  // Define environment variables to be injected at build time
-  define: {
-    'process.env': process.env,
-  },
+    resolve: {
+      alias: {
+        assets: path.resolve(__dirname, './assets'),
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    // Define environment variables to be injected at build time
+    define: {
+      'process.env': process.env,
+    },
+  };
 });
