93d3866fd635b6a1e8b8c5b0a01d9d875b77a8eb
Evgeny Taktarov <je.mouth@gmail.com>
2026-01-16 19:21:48 +0100

    fix: revert iframe app css

--- src/iframe-demo-wrapper/iframe.tsx
+++ src/iframe-demo-wrapper/iframe.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { IframeApp } from '@/iframe-demo-wrapper/iframe-app';
-import '../styles/App.css';
+import { IframeApp } from './iframe-app';
+import './App.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // Use createRoot for React 18+
