107cd9d31d720cdf36027f3d5daa5b12a5fc1e24
Evgeny Taktarov <je.mouth@gmail.com>
2026-03-18 18:02:29 +0100

    feat: allow claim rewards from strategy

--- src/features/stv-strategy-pool/shared/hooks/index.ts
+++ src/features/stv-strategy-pool/shared/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './use-strategy-position';
 export * from './use-process-withdrawal';
 export * from './use-recover';
 export * from './use-boost-apy';
+export * from './use-claim-proxy-distribution';
